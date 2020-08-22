d778a4e53a9c641bfe8d88954d61aea943a96883
Karolina <karolinabudzik92@gmail.com>
2020-08-22 18:26:12 +0200

    Add loading selectors

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-var apiUrl = 'https://api.exchangeratesapi.io/latest?base=';
+let apiUrlAll = 'https://api.exchangeratesapi.io/latest';
+let apiUrl = 'https://api.exchangeratesapi.io/latest?base=';
+let defaultCurrency = 'USD';
+
+function loadSelectors() {
+    let currencySelect = document.querySelector('select');
+    fetch(apiUrlAll)
+        .then(response => response.json())
+        .then(response => {
+            let currencies = Object.keys(response.rates);
+            for (const index in currencies) {
+                let currency = (currencies[index]);
+                let option = document.createElement('option');
+                option.appendChild(document.createTextNode(currency));
+                option.value = currency;
+                currencySelect.appendChild(option);
+            }
+        })
+}
 
 function getRates(currency) {
     newApiUrl = apiUrl + currency;
@@ -8,7 +26,6 @@ function getRates(currency) {
             let innerHtml = '';
             for (const property in response.rates) {
                 if (response.rates.hasOwnProperty(property)) {
-                
                     const row = `1${response.base} = ${response.rates[property]}${property}`;
                     const rowHtml = `<p>${row}</p>`;
                     innerHtml += rowHtml;
@@ -18,11 +35,16 @@ function getRates(currency) {
         })
 }
 
-(() => {
-    let base = document.querySelector('select');
-
-    base.addEventListener('change', (event) => {
+function showRatesOnSelectorChange() {
+    let currencySelect = document.querySelector('select');
+    currencySelect.addEventListener('change', (event) => {
         let currency = event.target.value;
         getRates(currency);
     });
+}
+
+(() => {
+    loadSelectors();
+    getRates(defaultCurrency);
+    showRatesOnSelectorChange();
 })();
