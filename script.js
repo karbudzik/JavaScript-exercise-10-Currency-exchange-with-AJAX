6b6c8bd7a602cac01d6a4a1ca9d53723a3b242ca
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-08-24 12:39:39 +0200

    Improve display

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function updateDiagram(dates, rates){
     console.log(mychart);
     mychart.data.datasets[0].data = rates;
     mychart.data.labels = dates;
-    mychart.data.datasets[0].label = 'changed';
+    mychart.data.datasets[0].label = currencyTo;
 
     mychart.update();
 }
@@ -88,23 +88,10 @@ function displayDiagram(){
         data: {
             labels: dates,
             datasets: [{
-                label: '# of Votes',
+                label: currencyTo,
                 data: rates,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(255, 153, 153, 0.5)'
                 ],
                 borderWidth: 1
             }]
